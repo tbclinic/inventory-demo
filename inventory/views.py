4787779907a28204ba7c1940a3d9659270284cb3
Yuhao <yuhaofeng@businessclinic.tokyo>
2024-03-10 19:12:20 +0900

    init commit

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,29 @@
-from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from inventory.forms import UserRegisterForm
 
 
 class Index(TemplateView):
     template_name = 'inventory/index.html'
+
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)              # Bonded form with user-submitted data
+
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+
+        return render(request, 'inventory/signup.html', {'form': form})
