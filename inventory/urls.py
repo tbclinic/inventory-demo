4cdd451350ca0ded876e37b45d46ab5434aa9e84
Yuhao <yuhaofeng@businessclinic.tokyo>
2024-03-10 19:17:58 +0900

    init commit

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index, SignUpView
 
 urlpatterns = [
     path("", Index.as_view(), name='index'),
+    path("signup/", SignUpView.as_view(), name='signup')
 ]
