804e8ecac091f2304b3c8982376ec086c587cca1
Yuhao <yuhaofeng@businessclinic.tokyo>
2024-03-10 20:41:52 +0900

    init commit

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", Index.as_view(), name='index'),
     path("signup/", SignUpView.as_view(), name='signup'),
-    path('login.', auth_views.LogoutView.as_view(template_name='inventory/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
 ]
